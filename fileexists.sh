70b09e1b640650c1523eced72e273a68932b21b8
hemasuraj879 <your_email@example.com>
2024-04-19 11:10:35 +0530

    shell script files are pushing

---
+++ fileexists.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# THIS SCRIPT IS USED TO KNOW THE FILE STATUS EXISTS OR NOT
+
+
+# TSORING THE LOG FILE
+LOGSDIR=/tmp
+DATE=$(date +%Y-%m-%d)
+SCRIPT_NAME=$0
+LOGFILE=$LOGSDIR/$SCRIPT_NAME-$DATE.log
+
+# CHECKING WHETHERE ON ROOT USER OR NOT
+USERID=$(id -u) &>>$LOGFILE
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR: PLEASE SWITCH TO ROOT USER"
+    exit 1
+fi 
+
+FILE_PATH=$(/etc)
+if [ -e $FILE_PATH ]
+then   
+    echo "FILE ALREADY EXISTS"
+else
+    echo "FILE DOES NOT EXISTS"
+fi
